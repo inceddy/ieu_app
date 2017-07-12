ae4928d92ec424461fa234505f72a22512f525fe
Philipp Steingrebe <development@steingrebe.de>
2017-07-12 11:49:48 +0200

    Added support for ieu\Http 2.0.3

--- src/ieu/App.php
+++ src/ieu/App.php
@@ -8,7 +8,12 @@ use ieu\Http\Request;
 use ieu\Http\RouterProvider;
 use ieu\Http\Response;
 
+use InvalidArgumentException;
+
 class App extends Container\Container {
+
+	public const OPTIONS_ARRAY = 0;
+	public const OPTIONS_JSON = 1;
 
 	public function __construct(...$containers)
 	{
@@ -25,16 +30,44 @@ class App extends Container\Container {
 	 * Alias for controller services.
 	 * The controller providers can be accessed by `controllernameController`.
 	 *
-	 * @param  string $name                      The name of the controller service
-	 * @param  array  $dependenciesAndClassname  The dependencies and the classnmae of the controller
+	 * @param  string $name
+	 *    The name of the controller service
+	 * @param  array  $dependenciesAndClassname
+	 *    The dependencies and the classnmae of the controller
 	 *
 	 * @return self
 	 * 
 	 */
 	
-	public function controller($name, $dependenciesAndClassname) 
+	public function controller(string $name, $dependenciesAndClassname) 
 	{
 		return $this->service($name . 'Controller', $dependenciesAndClassname);
+	}
+
+	public function options(string $name, $options, int $type = self::OPTIONS_ARRAY)
+	{
+		switch ($type) {
+			case self::OPTIONS_ARRAY:
+				if (is_file($options)) {
+					$options = include($options);
+				}
+				break;
+
+			case self::OPTIONS_JSON:
+				if (is_file($options)) {
+					$options = file_get_contents($options);
+				}
+
+				$options = json_decode($options, true);
+				break;
+
+			default:
+				throw new InvalidArgumentException('Unkown option type');				
+		}
+
+		$this->constant($name . 'Options', $options);
+
+		return $this;
 	}
 
 
@@ -42,8 +75,8 @@ class App extends Container\Container {
 	{
 		$this->boot();
 		
-		if (!($response = $this['Router']->handle()) instanceof Response){
-			$response = new Response('Error: Router does not deliver Response object!', 500);
+		if (null === $response = $this['Router']->handle($this['Request'])) {
+			$response = new Response('Internal server error', 500);
 		}
 
 		$response->send();
